711b3417a3d53156e6844af03d4f2bc5cf011045
Helder Fonseca <helder2391@gmail.com>
2022-12-07 15:55:37 -0100

    change background image

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <div className='pb-10'>
-        <div className="pt-8 md:h-screen px-10 bg-no-repeat bg-cover bg-center bg-[url('../public/background-hero-mobile.png')] xl:bg-[url('../public/background-hero.png')]">
+        <div className="pt-8 md:h-screen px-10 bg-no-repeat bg-cover bg-center bg-[url('/background-hero-mobile.png')] xl:bg-[url('/background-hero.png')]">
          <Header></Header>
          <Top></Top>
         </div>
